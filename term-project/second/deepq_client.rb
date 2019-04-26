fd52986cb3c8effdd419cf6219cfb9d141ae1dc1
tori-takashi <tori.toutoshi@gmail.com>
2019-04-26 14:52:45 +0800

    completed

--- term-project/second/deepq_client.rb
+++ term-project/second/deepq_client.rb
@@ -1,24 +1,3 @@
-# Requirements
-#
-# You need to implement the following functions for DDS :
-#     DDS implemented by students should create their own directory to save data information.
-#     Users can register accounts.
-#     A seller can upload data information to the directory created by DDS.
-#     A buyer can search data on DDS, which consists of all sellers’ data information.
-#     A buyer can browse the data information of all available data (not the deleted ones).
-#     When an EAS is established between a buyer and a seller, the EAS is deployed by DDS on Ethereum.
-#     A buyer or a seller can browse all EAS’s that he/she has deployed or revoked.
-#     An EAS is invoked by the buyer to access the data, and either a buyer or a seller can revoke an EAS.
-#
-# Rules
-# You should follow the rules below to implement this system:
-#
-#     You can create one directory, all the left operations should use the same directory.
-#     Only registered provider can create data entry.
-#     Each data certificate can be used to create only one data entry. It can only be used once.
-#     EAS is deployed by the platform after platform receive the agreements from both buyer and seller. The userID of the API is consumerID.
-#     Only provider and consumer of the EAS that has been deployed can revoke the deal.
-#     Those deals that are already revoked should not be revoked twice.
 require 'httpclient'
 require 'json'
 
@@ -37,109 +16,13 @@ class DeepqClient
     end
   end
 
-  def deepq_connect_test
-    response = @client.get(@@DEEPQ_URL)
-    HTTP::Status.successful?(response.code)
-  end
-
-  def check_response_status(response)
-    if HTTP::Status.successful?(response.code)
-      puts '200 OK response received'
-    else
-      puts "#{response.code} ERROR request failed"
-      error_data = JSON.parse(response.body)
-      puts error_data['error']
-    end
-    HTTP::Status.successful?(response.code)
-  end
-
   def start_console
     puts <<-'EOS'
 ************************************
 ***********DEEPQ CONSOLE************
 ************************************
     EOS
-
-    loop do
-      puts <<-"EOS"
--------------Main Menu--------------
-plese type a number from menu below
-
-0. exit
-1. user
-2. directory
-      EOS
-
-      selected_num = gets.to_i
-
-      case selected_num
-
-      when 0
-        break
-
-      when 1
-        puts <<-EOS
-0. back
-1. create user
-        EOS
-
-        selected_num = gets.to_i
-
-        case selected_num
-        when 0
-          next
-        when 1
-          create_user
-        end
-
-      when 2
-        puts <<-EOS
-0. back
-1. create directory
-        EOS
-
-        selected_num = gets.to_i
-
-        case selected_num
-        when 0
-          next
-        when 1
-          create_directory
-        end
-      end
-    end
-  end
-
-  def purified_output(hash)
-    hash.each do |k, v|
-      puts "#{k} : #{v}"
-    end
-  end
-
-  def request(method, url, params)
-    response = @client.post(url, body: params) if method == 'get'
-    response = @client.post(url, body: params) if method == 'post'
-
-    if check_response_status(response)
-      result_data = JSON.parse(response.body)
-
-      purified_output(result_data['result'])
-    end
-  end
-
-  def build_params(request_params)
-    params = {}
-    request_params.each do |request_param|
-      params[request_param] = ''
-    end
-
-    params_size = params.size
-
-    params.each_with_index do |(_param_key, _param_value), i|
-      puts "#{i + 1}/#{params_size}. please type #{_param_key}"
-      param_value = gets
-      params[_param_key] = param_value.strip!
-    end
+    main_menu
   end
 
   def create_directory
@@ -212,6 +95,148 @@ plese type a number from menu below
 
     params = build_params(request_params)
     request('get', url, params)
+  end
+
+  def main_menu
+    loop do
+      puts <<-"EOS"
+-------------Main Menu--------------
+plese type a number from menu below
+
+0. exit
+1. user
+2. directory
+3. data entry
+4. EAS
+      EOS
+
+      case selected_num = gets.to_i
+      when 0
+        break
+      when 1
+        user_menu
+      when 2
+        directory_menu
+      when 3
+        data_entry_menu
+      when 4
+        eas_menu
+      else
+        puts 'please type a number'
+        next
+      end
+    end
+  end
+
+  def user_menu
+    puts <<-EOS
+0. back
+1. create user
+    EOS
+
+    case selected_num = gets.to_i
+    when 1
+      create_user
+    end
+   end
+
+  def directory_menu
+    puts <<-EOS
+0. back
+1. create directory
+    EOS
+
+    case selected_num = gets.to_i
+    when 1
+      create_directory
+    end
+  end
+
+  def data_entry_menu
+    puts <<-EOS
+0. back
+1. create data entry
+2. count data entry
+3. find data entry by index
+4. find data entry by data certificate
+    EOS
+
+    case selected_num = gets.to_i
+    when 1
+      create_data_entry
+    when 2
+      count_data_entry
+    when 3
+      get_data_entry_by_index
+    when 4
+      get_data_entry_by_data_certificate
+    end
+end
+
+  def eas_menu
+    puts <<-EOS
+0. back
+1. create eas
+2. revoke eas
+3. get eas information
+    EOS
+
+    case selected_num = gets.to_i
+    when 1
+      create_eas
+    when 2
+      revoke_eas
+    when 3
+      get_eas
+    end
+  end
+
+  def build_params(request_params)
+    params = {}
+    request_params.each do |request_param|
+      params[request_param] = ''
+    end
+
+    params_size = params.size
+
+    params.each_with_index do |(_param_key, _param_value), i|
+      puts "#{i + 1}/#{params_size}. please type #{_param_key}"
+      param_value = gets
+      params[_param_key] = param_value.strip!
+    end
+  end
+
+  def deepq_connect_test
+    response = @client.get(@@DEEPQ_URL)
+    HTTP::Status.successful?(response.code)
+  end
+
+  def check_response_status(response)
+    if HTTP::Status.successful?(response.code)
+      puts '200 OK response received'
+    else
+      puts "#{response.code} ERROR request failed"
+      error_data = JSON.parse(response.body)
+      puts error_data['error']
+    end
+    HTTP::Status.successful?(response.code)
+  end
+
+  def request(method, url, params)
+    response = @client.post(url, body: params) if method == 'get'
+    response = @client.post(url, body: params) if method == 'post'
+
+    if check_response_status(response)
+      result_data = JSON.parse(response.body)
+
+      purified_output(result_data['result'])
+    end
+  end
+
+  def purified_output(hash)
+    hash.each do |k, v|
+      puts "#{k} : #{v}"
+    end
   end
 end
 
